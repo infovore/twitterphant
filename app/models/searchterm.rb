9c3fbf920c5179dd4ae94345c297de204e18fcaa
Tom Armitage <tom@infovore.org>
2012-07-30 10:18:21 +0100

    Method to slugify this.

--- app/models/searchterm.rb
+++ app/models/searchterm.rb
@@ -6,4 +6,8 @@ class Searchterm < ActiveRecord::Base
       Tweet.create_status_if_nonexsitent_for_searchterm(status, self)
     end
   end
+
+  def slugify
+    text.strip.downcase.gsub(/\s*[^A-Za-z0-9\.\-]\s*/, '_')
+  end
 end
